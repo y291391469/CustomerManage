ce9d3947f0d508b98e61f17a50606202654552de
Momoka Fujino <fujino.momoka1994@gmail.com>
2026-07-08 10:21:45 +0900

    Update CustomerDao.java

--- src/main/java/dao/CustomerDao.java
+++ src/main/java/dao/CustomerDao.java
@@ -1,12 +1,14 @@
 package dao;
 
+import static constants.MessageConstants.*;
+
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-import util.LogUtil;
 import customer.CustomerBean;
+import util.LogUtil;
 
 /**
  * 顧客管理DAO(オートコミット)
@@ -152,10 +154,37 @@ public class CustomerDao extends BaseDao {
      */
     public String add(CustomerBean customer) {
         LogUtil.println(this.getClass().getSimpleName() + "#add");
+        
+        String errMessage = null;
+        PreparedStatement pstmt = null;
+        String strSql = "INSERT INTO customer (id,name,zip,address1,address2,tel,fax,email)"
+        		+ "VALUES(sequence_customer_id.NEXTVAL,?,?,?,?,?,?,?)";
 
-        // TODO 未実装
+        try {
+            open();
+            pstmt = conn.prepareStatement(strSql);
+            pstmt.setString(1, customer.getName());
+            pstmt.setString(2, customer.getZip());
+            pstmt.setString(3, customer.getAddress1());
+            pstmt.setString(4, customer.getAddress2());
+            pstmt.setString(5, customer.getTel());
+            pstmt.setString(6, customer.getFax());
+            pstmt.setString(7, customer.getEmail());
+            pstmt.executeUpdate();
+        } catch (ClassNotFoundException | SQLException e) {
+        	errMessage = e.getMessage();
+            LogUtil.printStackTrace(e);
+        } finally {
+            try {
+                pstmt.close();
+                close();
+            } catch (SQLException e) {
+            	errMessage = e.getMessage();
+                LogUtil.printStackTrace(e);
+            }
+        }
 
-        return null;
+        return errMessage;
     }
 
     /**
@@ -166,11 +195,42 @@ public class CustomerDao extends BaseDao {
     public String update(CustomerBean cutomer) {
         LogUtil.println(this.getClass().getSimpleName() + "#update");
 
-        // TODO 未実装
+        String errMessage = null;
+        PreparedStatement pstmt = null;
+        String strSql = "UPDATE CUSTOMER SET id=?,name=?,zip=?,address1=?,address2=?,"
+                + "tel=?,fax=?,email=? WHERE id=?";
+        
+        try {
+            pstmt = conn.prepareStatement(strSql);
+            pstmt.setInt(1, cutomer.getId());
+            pstmt.setString(2, cutomer.getName());
+            pstmt.setString(3, cutomer.getZip());
+            pstmt.setString(4, cutomer.getAddress1());
+            pstmt.setString(5, cutomer.getAddress2());
+            pstmt.setString(6, cutomer.getTel());
+            pstmt.setString(7, cutomer.getFax());
+            pstmt.setString(8, cutomer.getEmail());
+            pstmt.setInt(9, cutomer.getId());
 
-        return null;
+            int result = pstmt.executeUpdate();
+            if (result == 0) {
+                errMessage = MESSAGE_NO_EXIST_DATA_TO_UPDATE;
+            }
+        } catch (SQLException e) {
+            errMessage = e.getMessage();
+            LogUtil.printStackTrace(e);
+        } finally {
+            try {
+                pstmt.close();
+                close();
+            } catch (SQLException e) {
+                errMessage = e.getMessage();
+                LogUtil.printStackTrace(e);
+            }
+        }
+        return errMessage;
     }
-
+    
     /**
      * IDを指定して顧客情報テーブルから顧客情報を削除する
      * @param id 顧客情報テーブルのID
@@ -179,8 +239,31 @@ public class CustomerDao extends BaseDao {
     public String delete(int id) {
         LogUtil.println(this.getClass().getSimpleName() + "#delete");
 
-        // TODO 未実装
+        String errMessage = null;
+        PreparedStatement pstmt = null;
+        String strSql = "DELETE from CUSTOMER WHERE id=?";
 
-        return null;
+        try {
+            pstmt = conn.prepareStatement(strSql);
+            pstmt.setInt(1, id);
+
+            int result = pstmt.executeUpdate();
+            if (result == 0) {
+                errMessage = MESSAGE_NO_EXIST_DATA_TO_DELETE;
+            }
+        } catch (SQLException e) {
+            errMessage = e.getMessage();
+            LogUtil.printStackTrace(e);
+        } finally {
+            try {
+                pstmt.close();
+                close();
+            } catch (SQLException e) {
+                errMessage = e.getMessage();
+                LogUtil.printStackTrace(e);
+            }
+        }
+        return errMessage;
     }
+
 }
